7ac0757731cf49e5b59124290c388fe4bcf52061
mjkim <mjkim@12cm.co.kr>
2023-12-12 18:46:22 +0900

    feat: JPA Auditing 허용
    - CommonApplication 에 `EnableJpaAuditing` 어노테이션 추가
    - JPA 의존성을 메인 build.gradle 로 이동

--- common/src/main/java/com/letter/CommonApplication.java
+++ common/src/main/java/com/letter/CommonApplication.java
@@ -2,8 +2,10 @@ package com.letter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class  CommonApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
