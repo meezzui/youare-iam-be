67cf5cf284f9032c304ea19912d721d70c1c036b
jan <samjan1127@gmail.com>
2023-12-13 02:59:42 +0900

    feat: 질문 선택 기능 구현 #9
    - Question ID 검증
    - 선택했던 질문 선택 불가
    - DB에 데이터 저장

--- storage/src/main/java/com/letter/question/repository/QuestionRepository.java
+++ storage/src/main/java/com/letter/question/repository/QuestionRepository.java
@@ -1,10 +1,13 @@
 package com.letter.question.repository;
 
 import com.letter.question.entity.Question;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionCustomRepository {
-
+    Optional<Question> findQuestionById(@NotNull Long id);
 }
