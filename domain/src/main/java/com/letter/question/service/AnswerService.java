0a819c980329bf359bfc9dbe2e9872578eb5fd32
jan <samjan1127@gmail.com>
2023-12-21 16:22:40 +0900

    fix: 답변 등록 검증 추가 #77
    - 답변 등록 시 사용자가 선택된 질문을 등록한 커플에 속하는지 검증하는 부분 추가

--- domain/src/main/java/com/letter/question/service/AnswerService.java
+++ domain/src/main/java/com/letter/question/service/AnswerService.java
@@ -2,7 +2,9 @@ package com.letter.question.service;
 
 import com.letter.exception.CustomException;
 import com.letter.exception.ErrorCode;
+import com.letter.member.entity.Couple;
 import com.letter.member.entity.Member;
+import com.letter.member.repository.CoupleCustomRepositoryImpl;
 import com.letter.member.repository.MemberRepository;
 import com.letter.question.dto.AnswerRequest;
 import com.letter.question.entity.Answer;
@@ -10,6 +12,7 @@ import com.letter.question.entity.SelectQuestion;
 import com.letter.question.repository.AnswerRepository;
 import com.letter.question.repository.SelectQuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,16 +22,26 @@ import org.springframework.stereotype.Service;
 public class AnswerService {
 
     private final MemberRepository memberRepository;
+    private final CoupleCustomRepositoryImpl coupleCustomRepository;
 
     private final AnswerRepository answerRepository;
     private final SelectQuestionRepository selectQuestionRepository;
 
+
+    @Value("${user.id}")
+    private String userId;
+
     public ResponseEntity<?> registerAnswer(AnswerRequest answerRequest) {
-        final Member member = memberRepository.findById("1").orElseThrow(
+        final Member member = memberRepository.findById(userId).orElseThrow(
                 () -> new RuntimeException(HttpStatus.UNAUTHORIZED.name())
         );
 
-        final SelectQuestion selectQuestion = selectQuestionRepository.findById(answerRequest.getSelectQuestionId()).orElseThrow(
+        final Couple couple = coupleCustomRepository.findCoupleInMemberByMemberId(member.getId());
+        if (couple == null) {
+            throw new CustomException(ErrorCode.COUPLE_NOT_FOUND);
+        }
+
+        final SelectQuestion selectQuestion = selectQuestionRepository.findByIdAndCouple(answerRequest.getSelectQuestionId(), couple).orElseThrow(
                 () -> new CustomException(ErrorCode.SELECT_QUESTION_NOT_FOUND)
         );
 
@@ -38,7 +51,7 @@ public class AnswerService {
             throw new CustomException(ErrorCode.ALREADY_ANSWER);
         }
 
-        answerRepository.save(new Answer(member, selectQuestion, answerRequest.getAnswer()));
+        answerRepository.save(new Answer(couple, member, selectQuestion, answerRequest.getAnswer()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
