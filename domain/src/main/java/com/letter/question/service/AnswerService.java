2401e605dfadbb5ec44b3dbcd4e6ad81cce5d8c8
jan <samjan1127@gmail.com>
2023-12-22 05:07:13 +0900

    feat: 답변 등록 렌더링 관련 데이터 조회
    - question 응답 DTO 생성
    - Question contents만 조회하는 커스텀 쿼리 생성
    - 조회 서비스 로직 구현
    - OpenAPI 설정 추가

--- domain/src/main/java/com/letter/question/service/AnswerService.java
+++ domain/src/main/java/com/letter/question/service/AnswerService.java
@@ -7,9 +7,11 @@ import com.letter.member.entity.Member;
 import com.letter.member.repository.CoupleCustomRepositoryImpl;
 import com.letter.member.repository.MemberRepository;
 import com.letter.question.dto.AnswerRequest;
+import com.letter.question.dto.QuestionContentsResponse;
 import com.letter.question.entity.Answer;
 import com.letter.question.entity.SelectQuestion;
 import com.letter.question.repository.AnswerRepository;
+import com.letter.question.repository.QuestionCustomRepositoryImpl;
 import com.letter.question.repository.SelectQuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,10 +28,30 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
     private final SelectQuestionRepository selectQuestionRepository;
+    private final QuestionCustomRepositoryImpl questionCustomRepository;
 
 
     @Value("${user.id}")
     private String userId;
+
+    public ResponseEntity<QuestionContentsResponse> getAnswersQuestion(Long selectedQuestionId) {
+        final Member member = memberRepository.findById(userId).orElseThrow(
+                () -> new RuntimeException(HttpStatus.UNAUTHORIZED.name())
+        );
+
+        final Couple couple = coupleCustomRepository.findCoupleInMemberByMemberId(member.getId());
+        if (couple == null) {
+            throw new CustomException(ErrorCode.COUPLE_NOT_FOUND);
+        }
+
+        final QuestionContentsResponse questionContentsResponse = questionCustomRepository.findQuestionContentsBySelectQuestionIdAndCouple(selectedQuestionId, couple);
+
+        if (questionContentsResponse == null) {
+            throw new CustomException(ErrorCode.SELECT_QUESTION_NOT_FOUND);
+        }
+
+        return ResponseEntity.ok(questionContentsResponse);
+    }
 
     public ResponseEntity<?> registerAnswer(AnswerRequest answerRequest) {
         final Member member = memberRepository.findById(userId).orElseThrow(
---
+++ storage/src/main/java/com/letter/question/dto/QuestionContentsResponse.java
@@ -0,0 +1,12 @@
+package com.letter.question.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionContentsResponse {
+
+    private String question;
+
+}
