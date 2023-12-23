2695569618e4537ee2b4626df1219e700dd1c9eb
mjkim <mjkim@12cm.co.kr>
2023-12-24 00:00:27 +0900

    feat: 사용자 조회 커스텀 어노테이션 #90
    - 커스텀 할 어노테이션의 이름으로 인터페이스 생성
    - resolver 파일 생성
    - WebConfig에 resolver 등록

--- common/src/main/java/com/letter/config/WebConfig.java
+++ common/src/main/java/com/letter/config/WebConfig.java
@@ -1,17 +1,23 @@
 package com.letter.config;
 
 import com.letter.interceptor.LoginCheckInterceptor;
+//import com.letter.interceptor.LoginUserArgumentResolver;
+import com.letter.interceptor.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginCheckInterceptor loginCheckInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry){
@@ -38,5 +44,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api-docs") // 스웨거 쪽 제외
                 .excludePathPatterns("/api/v1/members/kakao/callback"); // 로그인 제외
 
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+        resolvers.add(loginUserArgumentResolver);
     }
 }
