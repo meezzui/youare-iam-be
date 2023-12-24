4616ed159ee55ab70628eaa0f9475ba1044bcd81
jan <samjan1127@gmail.com>
2023-12-24 20:30:44 +0900

    refactor: 테스트용 API 삭제

--- common/src/main/java/com/letter/aws/EbHealthCheck.java
+++ common/src/main/java/com/letter/aws/EbHealthCheck.java
@@ -1,24 +1,14 @@
 package com.letter.aws;
 
-import com.letter.jwt.JwtProvider;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class EbHealthCheck {
-
-    private final JwtProvider jwtProvider;
-
     @GetMapping("/")
     public ResponseEntity<String> doHealthCheck() {
         return ResponseEntity.ok("Health Check OK");
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return jwtProvider.getSECRET();
-    }
 }
