f60aeb7ef1818e7755aab279d26e7c3593ce1488
jan <samjan1127@gmail.com>
2024-01-15 03:22:40 +0900

    feat: Refresh token 발급 #12
    - ErrorCode enum에 code 추가
    - JwtProvider에 Refresh token 발급 메서드 추가
    - 로그인 관련 메서드에 Refresh token 발급하고 DB에 저장하는 로직 추가
    - DB에 저장한 Refresh token으로 새로운 Access token 발급 하는 API 생성

--- storage/src/main/java/com/letter/member/repository/MemberCustomRepositoryImpl.java
+++ storage/src/main/java/com/letter/member/repository/MemberCustomRepositoryImpl.java
@@ -1,13 +1,13 @@
 package com.letter.member.repository;
 
+import com.letter.member.entity.Member;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.QueryException;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import static com.letter.member.entity.QMember.member;
 
@@ -17,19 +17,19 @@ import static com.letter.member.entity.QMember.member;
 public class MemberCustomRepositoryImpl implements MemberCustomRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    private final EntityManager entityManager;
 
-    public String findIdByKakaId(Long id) {
+    public Optional<Member> findIdByKakaoId(Long kakaoId) {
         try{
-            return jpaQueryFactory
-                    .select(member.id)
+            return Optional.ofNullable(jpaQueryFactory
+                    .select(member)
                     .from(member)
-                    .where(member.kakaoId.eq(id))
-                    .fetchOne();
+                    .where(member.kakaoId.eq(kakaoId))
+                    .fetchOne());
         }
         catch (QueryException q){
-            log.info("쿼리 실패");
+            log.error("쿼리 실패");
             return null;
         }
     }
+
 }
