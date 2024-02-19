f763a77c40f78a5ecef356a5b8aac3f5a5e1396f
jan <samjan1127@gmail.com>
2024-02-19 15:50:17 +0900

    !HOTFIX: CORS 허용 오리진 제한으로 보안 강화

--- common/src/main/java/com/letter/config/WebConfig.java
+++ common/src/main/java/com/letter/config/WebConfig.java
@@ -1,9 +1,9 @@
 package com.letter.config;
 
 import com.letter.interceptor.LoginCheckInterceptor;
-//import com.letter.interceptor.LoginUserArgumentResolver;
 import com.letter.interceptor.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -19,21 +19,29 @@ public class WebConfig implements WebMvcConfigurer {
     private final LoginCheckInterceptor loginCheckInterceptor;
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
+    @Value("${origin.frontend.prod}")
+    private String frontendProdOrigin;
+    @Value("${origin.bambam}")
+    private String bambamOrigin;
+    @Value("${origin.biyamn}")
+    private String biyamnOrigin;
+
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry){
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        // TODO swagger는 모든 Origin에 열어놓기
         corsRegistry.addMapping("/**")
-                .allowedOriginPatterns("*")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(frontendProdOrigin, bambamOrigin, biyamnOrigin)
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders("Custom-Header","Authorization") // 나가는 헤더 설정
 //                .allowedHeaders("Access-Control-Request-Headers") // 클라이언트에서 헤더에서 접근 가능하게 하기 위한 설정
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization") // 나가는 헤더 설정
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 
     /**
      * Controller method를 실행하기 전에 LoginCheckInterceptor를 실행하게 된다.
+     *
      * @param registry
      */
     @Override
@@ -47,7 +55,7 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
     }
 }
